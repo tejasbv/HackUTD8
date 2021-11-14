4142ef57e2ec41594046b43c95b3b3eaff2878f9
Tejas Bogguram Vasudev <txb180006@utdallas.edu>
2021-11-14 04:20:56 -0500

    minor has errors

---
+++ statefarm/src/components/CollisionType.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const crash_types {
+    
+}
+export default class CollisionType extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+           type: 0
+        }
+      }
+    render(){
+
+    }
+}
